78f096528fb6f006d2dba20b7d835b6c29b624d1
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-24 14:26:54 +0300

    Task 22 таблица квадратов

---
+++ Task 22_tableOFsquare/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите N");
+int num = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+while (c<=num)
+{
+   Console.Write(c*c); // or Console.WriteLine(c*c);
+   c++; 
+}
